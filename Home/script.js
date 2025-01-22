c77e8f37beae816a34fec0ad953125b591e8defc
Mohamed Alwakil <736736+MohamedDiaa@users.noreply.github.com>
2025-01-22 18:55:07 +0100

    add logo, refactor index page

--- Home/script.js
+++
@@ -1,8 +0,0 @@
-// تحديد الزر والقائمة
-const menuToggle = document.querySelector('.menu-toggle');
-const menu = document.querySelector('.menu');
-
-// إضافة حدث عند النقر على الزر
-menuToggle.addEventListener('click', () => {
-    menu.classList.toggle('active'); // تفعيل أو إلغاء القائمة
-});
