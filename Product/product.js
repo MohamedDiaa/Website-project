d2c1d65b74b5dfbe29ff4dc9b5479aed6505eb33
Mohamed Alwakil <736736+MohamedDiaa@users.noreply.github.com>
2025-01-22 20:55:50 +0100

    fix loading product pages

--- Product/product.js
+++ Product/product.js
@@ -6,14 +6,19 @@ var product = [
             wiping, new colonization can occur. 99% of the time, a surface is exposed to viruses and germs until
             the next disinfection occurs. What if there was a surface that could defend itself against certain
             viruses around the clock? Our antimicrobial coating inactivates the Sars cov-2 coronavirus 10x
-            faster, providing additional protection against the communicable disease.`
+            faster, providing additional protection against the communicable disease.`,
+
+        "img": "/assets/AdobeStock_342983353-1-min.webp"
+
     },
     {
         "title": `Nano coating`,
         "body": `Nano” (Greek: dwarf) is the part of a billionth of a meter. Ultra-thin coatings prevent limescale
             from forming on shower stalls in the bathroom, for example, as with nano glass sealing, or simply
             allow wine to roll off upholstery and T-shirts. Nano sealings have no effect on UV radiation – wood
-            undergoes its characteristic aging as desired, but remains free of mold and moss.`
+            undergoes its characteristic aging as desired, but remains free of mold and moss.`,
+        "img": "/assets/kermikbeschichtung-auto.jpg.pagespeed.ce.AjTXKsgJ2w.jpg"
+
     }
     ,
     {
@@ -23,6 +28,27 @@ var product = [
             ecologically sustainable solution that works in wind and weather. Sprayed on at a distance of 30
             centimeters, our impregnation spray technology works reliably after drying and is suitable for all
             textiles and outdoor shoes. Gore Tex and Sympatex membranes can be re-impregnated once the effect
-            wears off.`
+            wears off.`,
+        "img": "/assets/versiegelung-nano-ceramic-schutz.jpg.pagespeed.ce.txYEbo6Eiw.jpg"
+
     }
 ];
+
+let link = window.location.href;
+let arr = link.split(`?`);
+let param = arr[arr.length - 1]
+let arr2 = param.split(`=`);
+
+let v = arr2[1];
+if (v != undefined && v < product.length) {
+
+    document.getElementById(`pro-title`).textContent = product[v].title;
+    document.getElementById(`pro-body`).textContent = product[v].body;
+    document.getElementById(`pro-img`).setAttribute(`src`, product[v].img);
+
+}
+else {
+    document.getElementById(`pro-title`).textContent = product[0].title;
+    document.getElementById(`pro-body`).textContent = product[0].body;
+    document.getElementById(`pro-img`).setAttribute(`src`, product[0].img);
+}
